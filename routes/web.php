219e7bfc330b95210271057da346c0206216c90b
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-08-19 02:26:37 +0400

    Fix About Blade preview route order

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\LegacyPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/{page}', LegacyPageController::class)->name('legacy.page');
 Route::view('/about-blade', 'pages.about')->name('about.blade');
+Route::get('/{page}', LegacyPageController::class)->name('legacy.page');
