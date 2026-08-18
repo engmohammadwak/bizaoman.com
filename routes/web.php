602144b2dd03f3af8a118441dedab43a2d4811d3
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-08-19 02:22:57 +0400

    Expose About page Blade migration safely

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 Route::get('/{page}', LegacyPageController::class)->name('legacy.page');
+Route::view('/about-blade', 'pages.about')->name('about.blade');
