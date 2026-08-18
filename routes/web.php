a4416c5c9af595c0c53b0fa95c8678f78ec2fdf1
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-08-19 02:30:57 +0400

    Activate About Blade page route

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,9 @@ use App\Http\Controllers\LegacyPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('/about', [AboutController::class, 'preview'])->name('about');
 Route::get('/about-blade', [AboutController::class, 'preview'])->name('about.blade');
+Route::get('/about-legacy', function () {
+    return response()->file(public_path('legacy/about/index.html'));
+})->name('about.legacy');
 Route::get('/{page}', LegacyPageController::class)->name('legacy.page');
